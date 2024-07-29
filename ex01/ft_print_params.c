35afdeeaee6ddd76c9b36d1f2df0630d9dcf99f2
Roberto Del Franco <rdel-fra@c3r13p4.42sp.org.br>
2024-07-29 12:23:59 -0300

    adicionando C06

---
+++ ex01/ft_print_params.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 12:09:45 by rdel-fra          #+#    #+#             */
+/*   Updated: 2024/07/29 11:20:20 by rdel-fra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 0;
+	if (argc)
+	{
+		while (argv[i + 1])
+		{
+			ft_putstr(argv[i + 1]);
+			i++;
+		}
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
