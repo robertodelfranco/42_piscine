8e80598c7983763ca628d57b1b9923eb7a46e1d0
Roberto <129216113+robertodelfranco@users.noreply.github.com>
2024-11-06 09:58:48 -0300

    Update ft_atoi.c

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -16,29 +16,23 @@ int	ft_atoi(char *str)
 {
 	int	i;
 	int	num;
-	int	c;
+	int	sign;
 
 	i = 0;
 	num = 0;
-	c = 0;
+	sign = 1;
 	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
-	while (str[i] != '\0')
+	if (str[i] == '-')
+		sign *= -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (str[i] == '-')
-			c++;
-		else if (str[i] >= '0' && str[i] <= '9')
-		{
-			num = num * 10 + (str[i] - '0');
-		}
-		else if (!((str[i] >= 48 && str[i] <= 57)
-				|| str[i] == '-' || str[i] == '+'))
-			break ;
+		num = num * 10 + (str[i] - '0');
 		i++;
 	}
-	if (c % 2 != 0)
-		num = num *(-1);
-	return (num);
+	return (num * sign);
 }
 
 
